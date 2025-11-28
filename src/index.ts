ace8650a11859726c8cc3344dcf2a86105eadcd7
Willow (GHOST) <git@willow.sh>
2025-11-28 06:14:52 +0000

    chore: pull in original

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {},
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {},
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const contentType = request.headers.get('content-type');
+		const ip = request.headers.get('cf-connecting-ip');
+		const url = new URL(request.url);
+
+		const body =
+			contentType == 'application/json' || url.pathname.endsWith('json')
+				? JSON.stringify({ ip })
+				: `${ip}`;
+
+		return new Response(body, {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+		});
+	},
 };
