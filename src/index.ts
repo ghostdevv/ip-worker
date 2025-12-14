2b984d9e680a7e64b8985bbc03ab859739ed26cd
Willow (GHOST) <git@willow.sh>
2025-12-14 20:21:35 +0000

    feat: v2 (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		const contentType = request.headers.get('content-type');
-		const ip = request.headers.get('cf-connecting-ip');
-		const url = new URL(request.url);
+import { AutoRouter, cors, json, text, type IRequest } from 'itty-router';
 
-		const body =
-			contentType == 'application/json' || url.pathname.endsWith('json')
-				? JSON.stringify({ ip })
-				: `${ip}`;
+function ip(request: IRequest) {
+	const accept = request.headers.get('accept');
+	const ip = request.headers.get('cf-connecting-ip');
 
-		return new Response(body, {
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		});
-	},
-};
+	switch (accept) {
+		case 'application/json':
+			return json({ ip });
+
+		case 'text/plain':
+		default:
+			return text(`${ip}`);
+	}
+}
+
+function cc(request: IRequest) {
+	const accept = request.headers.get('accept');
+	const cc = request.headers.get('cf-ipcountry');
+
+	switch (accept) {
+		case 'application/json':
+			return json({ cc });
+
+		case 'text/plain':
+		default:
+			return text(`${cc}`);
+	}
+}
+
+const { preflight, corsify } = cors();
+
+const router = AutoRouter({ before: [preflight], finally: [corsify] })
+	.get('/', ip)
+	.get('/ip', ip)
+	.get('/cc', cc);
+
+export default { fetch: router.fetch };
